95949ce1d9d7bef604fe9aa6c3bc68a74dad8c04
eejlny <eejlny@bristol.ac.uk>
2019-03-25 09:29:15 +0000

    crypto extensions added for CPU

--- zcu102/AES_host/aes_enc.h
+++ zcu102/AES_host/aes_enc.h
@@ -45,6 +45,69 @@ either expressed or implied, of the FreeBSD Project*/
 ////#define HW_ADDR_GPIO_INT 0x41200120
 ////#define fpga_mmio_gpio 0x48 /*location of status register in GPIO. wrte this to generate interrupt*/
 
+/* aes-arm.c - ARMv8 AES extensions using C intrinsics         */
+/*   Written and placed in public domain by Jeffrey Walton     */
+/*   Based on code from ARM, and by Johannes Schneiders, Skip  */
+/*   Hovsmith and Barry O'Rourke for the mbedTLS project.      */
+
+/* gcc -std=c99 -march=armv8-a+crypto aes-arm.c -o aes-arm.exe */
+
+/* Visual Studio 2017 and above supports ARMv8, but its not clear how to detect */
+/* it or use it at the moment. Also see http://stackoverflow.com/q/37244202,    */
+/* http://stackoverflow.com/q/41646026, and http://stackoverflow.com/q/41688101 */
+#if defined(__arm__) || defined(__aarch32__) || defined(__arm64__) || defined(__aarch64__) || defined(_M_ARM)
+# if defined(__GNUC__)
+#  include <stdint.h>
+# endif
+# if defined(__ARM_NEON) || defined(_MSC_VER)
+#  include <arm_neon.h>
+# endif
+/* GCC and LLVM Clang, but not Apple Clang */
+# if defined(__GNUC__) && !defined(__apple_build_version__)
+#  if defined(__ARM_ACLE) || defined(__ARM_FEATURE_CRYPTO)
+#   include <arm_acle.h>
+#  endif
+# endif
+#endif  /* ARM Headers */
+
+void aes_process_arm(const uint8_t key[], const uint8_t subkeys[], uint32_t rounds,
+                     const uint8_t input[], uint8_t output[], uint32_t length)
+{
+	while (length >= 16)
+	{
+		uint8x16_t block = vld1q_u8(input);
+
+		// AES single round encryption
+		block = vaeseq_u8(block, vld1q_u8(key));
+		// AES mix columns
+		block = vaesmcq_u8(block);
+
+		// AES single round encryption
+		block = vaeseq_u8(block, vld1q_u8(subkeys));
+		// AES mix columns
+		block = vaesmcq_u8(block);
+
+		for (unsigned int i=1; i<rounds-2; ++i)
+		{
+			// AES single round encryption
+			block = vaeseq_u8(block, vld1q_u8(subkeys+i*16));
+			// AES mix columns
+			block = vaesmcq_u8(block);
+		}
+
+		// AES single round encryption
+		block = vaeseq_u8(block, vld1q_u8(subkeys+(rounds-2)*16));
+		// Final Add (bitwise Xor)
+		block = veorq_u8(block, vld1q_u8(subkeys+(rounds-1)*16));
+
+		vst1q_u8(output, block);
+
+		input += 16; output += 16;
+		length -= 16;
+	}
+}
+
+
 void aes_enc(uint8_t *state,uint8_t *cipher,uint8_t ekey[240]);
 
 ////void aes_enc_hw(uint32_t *state,uint32_t *cipher,uint8_t ekey[240],int block_count,int *interrupt,int *status);
